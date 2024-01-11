b96794669f234356cb89b61fc08e5efb12d31cb6
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-11 23:25:19 +0900

    Title: 합성수 판별기, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/합성수 판별기/factorization-discriminator.java
+++ 240111/합성수 판별기/factorization-discriminator.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+	    int n;
+        n = sc.nextInt();
+        boolean satisfied = false;
 
-        int n = sc.nextInt();
-        boolean sat = false;
+        for(int i = 2; i < n; i++) {
+            // n의 약수가 있다면 합성수입니다.
+            if(n % i == 0)
+                satisfied = true;
+        }
 
-        if(n > 1 && n != 2 && n % 2 == 0){
-            sat = true;
-        }
-        if(sat == true){
-            System.out.print("C");
-        } else {
-            System.out.print("N");
-        }
+        //출력
+        if(satisfied == true)
+            System.out.println("C");
+        else
+            System.out.println("N");
     }
 }
