ae24a1a14ddcbb6023bdb873dab932adeaebe05f
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-16 20:32:03 +0900

    Title: 좌우 지그재그 출력, Time: 256ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240116/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 1;
+
+        for(int i = 0; i < n; i++){
+            if(i % 2 != 0){
+                cnt += n;
+            }
+            for(int j = 1; j <= n; j++){
+                if(i % 2 == 0){
+                    System.out.print(cnt + " ");
+                    cnt++;
+                } else {
+                    System.out.print(cnt - j + " "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+}
