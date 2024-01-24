5de0f62b266409ffed3f3367c2f3c11216b81213
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-24 15:34:40 +0900

    Title: 완전수, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/완전수/perfect-number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+        
+
+        for(int i = n; i <= m; i++){
+            int sum = 0;
+            for(int j = 1; j < i; j++){
+                if(i % j == 0){
+                    sum += j;
+                }
+            }
+            if(sum == i){
+                cnt++;
+            }
+            
+
+        }System.out.print(cnt);
+    }
+}
