f1f10a71493cbe2aee3f82388e5eff505ddcaa22
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-24 16:21:47 +0900

    Title: 특정 숫자 도달하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240124/특정 숫자 도달하기/reaching-specific-number.java
+++ 240124/특정 숫자 도달하기/reaching-specific-number.java
@@ -11,7 +11,7 @@ public class Main {
 
         for(int i = 0; i < 10; i++){
             a[i] = sc.nextInt();
-            if(a[i] == 260){
+            if(a[i] >= 260){
                 break;
             }
             sum += a[i];
