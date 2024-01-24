e3c733762be1005aa4bafef408a08e091b5586e3
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-24 15:11:31 +0900

    Title: ab곱 n번 구하기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/ab곱 n번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+
+        for(int i = 0; i < n; i++){
+            int sum = 1;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for(int j = a; j <= b; j++){
+                sum *= j;
+            }
+            System.out.println(sum);
+        }
+    }
+}
