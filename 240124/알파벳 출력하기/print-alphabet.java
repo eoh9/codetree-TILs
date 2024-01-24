a6950038a69b1876141ccd4ed522547d5d3a3456
Erin Oh <62730155+eoh9@users.noreply.github.com>
2024-01-24 14:58:17 +0900

    Title: 알파벳 출력하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240124/알파벳 출력하기/print-alphabet.java
+++ 240124/알파벳 출력하기/print-alphabet.java
@@ -8,10 +8,16 @@ public class Main {
         int x = 'A';
 
         for(int i = n; i > 0; i--){
+            
             for(int j = 0; j <= n-i; j++){
+                
                 System.out.print((char)x);
+                if(x == 'Z'){
+                    x = (char)64;
+                }
                 x++;
             }
+            
             System.out.println();
         }
     }
